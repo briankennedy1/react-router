5af9eaf887deeedbebb4da151ac0537d3fce0faa
Brian Kennedy <briankennedy1@gmail.com>
2015-12-14 11:56:16 -0800

    React auth testing

--- examples/auth-flow/app.js
+++ examples/auth-flow/app.js
@@ -1,29 +1,29 @@
-import React from 'react'
-import { render } from 'react-dom'
-import { Router, Route, Link, History } from 'react-router'
-import { createHistory, useBasename } from 'history'
-import auth from './auth'
+import React from 'react';
+import { render } from 'react-dom';
+import { Router, Route, Link, History } from 'react-router';
+import { createHistory, useBasename } from 'history';
+import auth from './auth';
 
 const history = useBasename(createHistory)({
-  basename: '/auth-flow'
-})
+  basename: '/auth-flow',
+});
 
 const App = React.createClass({
   getInitialState() {
     return {
-      loggedIn: auth.loggedIn()
-    }
+      loggedIn: auth.loggedIn(),
+    };
   },
 
   updateAuth(loggedIn) {
     this.setState({
-      loggedIn: loggedIn
-    })
+      loggedIn: loggedIn,
+    });
   },
 
   componentWillMount() {
-    auth.onChange = this.updateAuth
-    auth.login()
+    auth.onChange = this.updateAuth;
+    auth.login();
   },
 
   render() {
@@ -42,86 +42,148 @@ const App = React.createClass({
         </ul>
         {this.props.children}
       </div>
-    )
-  }
-})
+    );
+  },
+});
 
 const Dashboard = React.createClass({
-  render() {
-    const token = auth.getToken()
+  getInitialState() {
+    return {
+      everything: auth.getEverything(),
+    };
+  },
 
+  TripleHandler() {
+    console.log(`Testing token state: ${this.state.everything.token}`);
+    $.ajax({
+      method: 'GET',
+      url: 'http://localhost:3000/v1/players/bondb001/batting/triples',
+      headers: {
+        'access-token': this.state.everything.token,
+        client: this.state.everything.client,
+        expiry: this.state.everything.expiry,
+        uid: this.state.everything.uid,
+        'token-type': 'Bearer',
+      },
+    }).done((data, textStatus, res) => {
+      this.setState({everything: {
+        token: res.getResponseHeader('access-token'),
+        client: res.getResponseHeader('client'),
+        expiry: res.getResponseHeader('expiry'),
+        uid: res.getResponseHeader('uid'),
+      }, });
+    }).fail(() => {
+      console.log('fail');
+    });
+  },
+
+  DoubleHandler() {
+    console.log(`Testing token state: ${this.state.everything.token}`);
+    $.ajax({
+      method: 'GET',
+      url: 'http://localhost:3000/v1/players/bondb001/batting/doubles',
+      headers: {
+        'access-token': this.state.everything.token,
+        client: this.state.everything.client,
+        expiry: this.state.everything.expiry,
+        uid: this.state.everything.uid,
+        'token-type': 'Bearer',
+      },
+    }).done((data, textStatus, res) => {
+      this.setState({everything: {
+        token: res.getResponseHeader('access-token'),
+        client: res.getResponseHeader('client'),
+        expiry: res.getResponseHeader('expiry'),
+        uid: res.getResponseHeader('uid'),
+      }, });
+    }).fail(() => {
+      console.log('fail');
+    });
+  },
+
+  render() {
     return (
       <div>
         <h1>Dashboard</h1>
         <p>You made it!</p>
-        <p>{token}</p>
+        <button onClick={this.TripleHandler}>Triples</button>
+        <button onClick={this.DoubleHandler}>Doubles</button>
+        <p>Token: {this.state.everything.token}</p>
+        <p>Client: {this.state.everything.client}</p>
+        <p>Expiry: {this.state.everything.expiry}</p>
+        <p>UID: {this.state.everything.uid}</p>
       </div>
-    )
-  }
-})
+    );
+  },
+});
 
 const Login = React.createClass({
-  mixins: [ History ],
+  mixins: [History],
 
   getInitialState() {
     return {
-      error: false
-    }
+      error: false,
+    };
   },
 
   handleSubmit(event) {
-    event.preventDefault()
+    event.preventDefault();
 
-    const email = this.refs.email.value
-    const pass = this.refs.pass.value
+    const email = this.refs.email.value;
+    const pass = this.refs.pass.value;
 
     auth.login(email, pass, (loggedIn) => {
       if (!loggedIn)
-        return this.setState({ error: true })
+        return this.setState({ error: true });
 
-      const { location } = this.props
+      const { location } = this.props;
 
       if (location.state && location.state.nextPathname) {
-        this.history.replaceState(null, location.state.nextPathname)
+        this.history.replaceState(null, location.state.nextPathname);
       } else {
-        this.history.replaceState(null, '/')
+        this.history.replaceState(null, '/');
       }
-    })
+    });
   },
 
   render() {
     return (
       <form onSubmit={this.handleSubmit}>
-        <label><input ref="email" placeholder="email" defaultValue="joe@example.com" /></label>
+        <label><input ref="email" placeholder="email" defaultValue="bk3@bk3.com" /></label>
         <label><input ref="pass" placeholder="password" /></label> (hint: password1)<br />
         <button type="submit">login</button>
         {this.state.error && (
           <p>Bad login information</p>
         )}
       </form>
-    )
-  }
-})
+    );
+  },
+});
 
 const About = React.createClass({
   render() {
-    return <h1>About</h1>
-  }
-})
+    return (
+      <div>
+        <h1>About</h1>
+        <p>Aboooooot it baby.</p>
+      </div>
+    );
+  },
+});
 
 const Logout = React.createClass({
   componentDidMount() {
-    auth.logout()
+    auth.logout();
   },
 
   render() {
-    return <p>You are now logged out</p>
-  }
-})
+    return <p>You are now logged out</p>;
+  },
+});
 
 function requireAuth(nextState, replaceState) {
   if (!auth.loggedIn())
-    replaceState({ nextPathname: nextState.location.pathname }, '/login')
+    replaceState({ nextPathname: nextState.location.pathname }, '/login');
 }
 
 render((
@@ -133,4 +195,4 @@ render((
       <Route path="dashboard" component={Dashboard} onEnter={requireAuth} />
     </Route>
   </Router>
-), document.getElementById('example'))
+), document.getElementById('example'));
--- examples/auth-flow/auth.js
+++ examples/auth-flow/auth.js
@@ -1,49 +1,69 @@
 module.exports = {
   login(email, pass, cb) {
-    cb = arguments[arguments.length - 1]
+    cb = arguments[arguments.length - 1];
     if (localStorage.token) {
-      if (cb) cb(true)
-      this.onChange(true)
-      return
+      if (cb) cb(true);
+      this.onChange(true);
+      return;
     }
-    pretendRequest(email, pass, (res) => {
+
+    apiRequest(email, pass, (res) => {
       if (res.authenticated) {
-        localStorage.token = res.token
-        if (cb) cb(true)
-        this.onChange(true)
+        localStorage.token = res.token;
+        localStorage.client = res.client;
+        localStorage.expiry = res.expiry;
+        localStorage.uid = res.uid;
+        if (cb) cb(true);
+        this.onChange(true);
       } else {
-        if (cb) cb(false)
-        this.onChange(false)
+        if (cb) cb(false);
+        this.onChange(false);
       }
-    })
+    });
   },
 
   getToken() {
-    return localStorage.token
+    return localStorage.token;
+  },
+
+  getEverything() {
+    return localStorage;
   },
 
   logout(cb) {
-    delete localStorage.token
-    if (cb) cb()
-    this.onChange(false)
+    delete localStorage.token;
+    delete localStorage.client;
+    delete localStorage.expiry;
+    delete localStorage.uid;
+    if (cb) cb();
+    this.onChange(false);
   },
 
   loggedIn() {
-    return !!localStorage.token
+    return !!localStorage.token;
   },
 
-  onChange() {}
-}
+  onChange() {
+  },
 
-function pretendRequest(email, pass, cb) {
-  setTimeout(() => {
-    if (email === 'joe@example.com' && pass === 'password1') {
-      cb({
-        authenticated: true,
-        token: Math.random().toString(36).substring(7)
-      })
-    } else {
-      cb({ authenticated: false })
-    }
-  }, 0)
+};
+
+function apiRequest(email, pass, cb) {
+  $.ajax({
+    method: 'POST',
+    url: 'http://localhost:3000/v1/auth/sign_in',
+    data: { email: email, password: pass },
+  }).done((data, textStatus, jqXHR) => {
+    cb({
+      authenticated: true,
+      token: jqXHR.getResponseHeader('access-token'),
+      client: jqXHR.getResponseHeader('client'),
+      expiry: jqXHR.getResponseHeader('expiry'),
+      uid: jqXHR.getResponseHeader('uid'),
+      'token-type': 'bearer',
+    });
+  }).fail(() => {
+    cb({ authenticated: false });
+  });
+
 }
